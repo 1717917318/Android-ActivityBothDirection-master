5e2bdf8061880f7964074f0bb4d3738fa319fcdb
jianting <1727917318@qq.com>
2018-05-02 21:45:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/py/project11/AnotherActivity.java
@@ -0,0 +1,41 @@
+package com.example.py.project11;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+public class AnotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+
+        Intent intent=getIntent();
+        String name=intent.getStringExtra("name");
+        Integer age=intent.getIntExtra("age", 21);
+        Toast.makeText(this,name+age,Toast.LENGTH_LONG).show();
+        Button button2=(Button) findViewById(R.id.buttonReturn);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_main);
+            }
+        });
+    }
+
+}
